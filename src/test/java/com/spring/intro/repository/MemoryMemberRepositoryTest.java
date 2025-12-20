696a89be6812dd44a4d89158d208241d07cc5eab
handaehee93 <hdh8990@naver.com>
2025-12-20 10:00:20 +0900

    회원 리포지토리 테스트 케이스 작성

--- src/test/java/com/spring/intro/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/com/spring/intro/repository/MemoryMemberRepositoryTest.java
@@ -1,13 +1,36 @@
 package com.spring.intro.repository;
 
+import com.spring.intro.domain.Member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 
 class MemoryMemberRepositoryTest {
 
+    MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        ((MemoryMemberRepository)memberRepository).clearStore();
+    }
+
     @Test
     void save() {
+        Member member = new Member();
+        member.setName("hdh");
+
+        memberRepository.save(member);
+        Member result = memberRepository.findById(member.getId()).get();
+
+        assertThat(member).isEqualTo(result);
+
+
     }
 
     @Test
@@ -16,10 +39,32 @@ class MemoryMemberRepositoryTest {
 
     @Test
     void findAll() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memberRepository.save(member1);
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberRepository.save(member2);
+        //when
+        List<Member> result = memberRepository.findAll();
+        //then
+        assertThat(result.size()).isEqualTo(2);
     }
 
     @Test
     void findByName() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        memberRepository.save(member1);
+        Member member2 = new Member();
+        member2.setName("spring2");
+        memberRepository.save(member2);
+        //when
+        Member result = memberRepository.findByName("spring1").get();
+        //then
+        assertThat(result).isEqualTo(member1);
     }
 
     @Test
